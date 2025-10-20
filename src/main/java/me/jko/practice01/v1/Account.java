6c7bef545685f8f3449c686044f8a0f34cece20f
junhee-ko <junheee.ko@gmail.com>
2025-10-20 23:39:09 +0900

    add account class before refactor

---
+++ src/main/java/me/jko/practice01/v1/Account.java
@@ -0,0 +1,11 @@
+package me.jko.practice01.v1;
+
+public class Account {
+    public String owner;
+    public int balance;
+
+    public Account(String owner, int balance) {
+        this.owner = owner;
+        this.balance = balance;
+    }
+}
